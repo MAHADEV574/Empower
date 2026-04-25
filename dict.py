ce85560af2435a9737ce55fea6d25bcacdad352c
MAHADEV574 <mahadevmahi574@gmail.com>
2026-04-25 12:07:27 +0530

    Add files via upload

---
+++ dict.py
@@ -0,0 +1,7 @@
+# Python
+# WAP= create a dictionary out of following list
+["yellow","red","blue","black","orange"]
+
+colors=["yellow","red","blue","black","orange"]
+color_dict={color:len(color)for color in colors}
+print(color_dict)
